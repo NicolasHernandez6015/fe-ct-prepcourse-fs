2a3cc8295019b593418422b63c2c70430c20f090
NicolasHernandez6015 <nicolashernandez6015@gmail.com>
2023-06-27 18:34:20 -0300

    M04

---
+++ Demo/bucle.js
@@ -0,0 +1,13 @@
+var suma = 0
+/* suma = suma +1;
+console.log(suma) */
+
+/* for (var i=0 ; i<5 ; i++){
+    suma = suma + i
+    console.log("Variable de interación", suma);
+} */
+
+while(suma < 6) {
+    suma = suma + 1;
+    console.log(suma)
+}
